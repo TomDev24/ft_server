f7a27245635ba6d0ac6c5d9d82493d46fba5121f
Devona Brittany <dbrittan@ge-g4.kzn.21-school.ru>
2021-01-06 20:09:41 +0300

    mysql is configured, wp is downloaded, configuration of nginx, wp, phpadmin left

--- Dockerfile
+++ Dockerfile
@@ -5,11 +5,12 @@ RUN apt-get update && apt-get install -y vim \
 	&& apt-get install -y php7.3-cli php7.3-fpm \
 	php7.3-mysql php7.3-json php7.3-opcache \
 	php7.3-mbstring php7.3-xml php7.3-gd php7.3-curl \
-	&& apt-get install -y mariadb-server
+	&& apt-get install -y mariadb-server \
+	&& apt-get install -y wget 
 
-COPY ./srcs/start_container.sh ./tmp
+COPY ./srcs/start_container.sh .
 COPY ./srcs/site_nginx.conf ./tmp
 COPY ./srcs/index.html /var/www/html/
 EXPOSE 80
 
-CMD bash ./tmp/start_container.sh
+CMD bash start_container.sh
--- srcs/start_container.sh
+++ srcs/start_container.sh
@@ -7,9 +7,22 @@ rm -rf /etc/nginx/sites-enabled/default
 
 # SSL
 mkdir /etc/nginx/ssl
-sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/site.key -out /etc/nginx/ssl/site.pem "/C=RU/ST=Tatar/L=Kazan/O=MyComp/CN=site.com"
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/site.key -out /etc/nginx/ssl/site.pem "/C=RU/ST=Tatar/L=Kazan/O=MyComp/CN=site.com"
 
 # MySQL
+mysql --user='root' --execute="CREATE DATABASE wp;"
+mysql --user='root' --execute="CREATE USER 'wp_user'@'localhost' IDENTIFIED BY 'wp_user'"
+mysql --user='root' --execute="GRANT ALL ON wp.* TO 'wp_user'@'localhost' IDENTIFIED BY 'wp_user'"
+mysql --user='root' --execute="FLUSH PRIVILEGES"
+
+# WordPress
+mkdir -p /var/www/html/site.com
+cd /tmp
+wget https://wordpress.org/latest.tar.gz
+tar xf latest.tar.gz
+mv /tmp/wordpress/* /var/www/html/site.com/
+cd ..
+
 
 
 nginx -g "daemon off;" 
